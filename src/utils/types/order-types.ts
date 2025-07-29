903c2e93fdc2387123199277657226f5f4a555ba
Ekaterina Iurkova <chertaika1439@yandex.ru>
2025-07-29 16:27:50 +0700

    docs: update README and configure gh-pages deploy

--- src/utils/types/order-types.ts
+++ src/utils/types/order-types.ts
@@ -1,4 +1,4 @@
-import { TIngredients, TUser } from '@utils/types';
+import { TUser } from '@utils/types';
 
 type TTimestamps = {
 	readonly createdAt: string;
@@ -8,7 +8,7 @@ type TTimestamps = {
 export type TOrderStatus = 'pending' | 'created' | 'done';
 
 export type TOrder = TTimestamps & {
-	ingredients: TIngredients;
+	ingredients: Array<string>;
 	name: string;
 	number: number;
 	owner: TUser & TTimestamps;
